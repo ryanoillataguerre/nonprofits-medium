b74862275a2408c86a57d8bff1f339cc440dd00c
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2021-07-07 11:00:01 -0700

    Dupe server for math-service

---
+++ services/math-service/src/index.ts
@@ -0,0 +1,6 @@
+import dotenv from 'dotenv';
+import server from './server';
+
+dotenv.config();
+const port = process.env.PORT || 8080;
+server().listen(port);
---
+++ services/math-service/src/server.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from 'express';
+import http from 'http';
+
+const app = () => {
+  const app = express();
+
+  app.use((_: Request, res: Response, next) => {
+    
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Credentials, Set-Cookie',
+    );
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Accept, Access-Control-Allow-Credentials, Cross-Origin',
+    );
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+  });
+
+  app.use(express.json());
+
+  // Routes
+  app.get('/health', (_, res) => res.status(200).send({ success: true }));
+  // All non-specified routes return 404
+  app.get('*', (_, res) => res.status(404).send('Not Found'));
+
+  const server = http.createServer(app);
+
+  server.on('listening', () => {
+    console.info(`Users service listening on port ${process.env.PORT}...`);
+  });
+
+  return server;
+};
+
+export default app;
